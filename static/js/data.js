cb089d45aaedc83d5f6e528cf22b931f2bfebb1a
Rob Phillips <robbie.phillips@lmco.com>
2019-04-30 21:03:57 -0400

    Add files via upload

---
+++ static/js/data.js
@@ -0,0 +1,129 @@
+var monsterdata = [{
+    init: "7",
+    race: "",
+    class: "",
+    type: "",
+    size: "",
+    dexmod: ""
+  },
+  {
+    init: "12",
+    race: "",
+    class: "",
+    type: "",
+    size: "",
+    dexmod: ""
+  },
+  {
+    init: "16",
+    race: "",
+    class: "",
+    type: "",
+    size: "",
+    dexmod: ""
+  },
+  {
+    init: "15",
+    race: "",
+    class: "",
+    type: "",
+    size: "",
+    dexmod: ""
+  },
+  {
+    init: "17",
+    race: "",
+    class: "",
+    type: "",
+    size: "",
+    dexmod: ""
+  },
+  {
+    init: "19",
+    race: "",
+    class: "",
+    type: "",
+    size: "",
+    dexmod: ""
+  },
+  {
+    init: "20",
+    race: "",
+    class: "",
+    type: "",
+    size: "",
+    dexmod: ""
+  },
+  {
+    init: "5",
+    race: "",
+    class: "",
+    type: "",
+    size: "",
+    dexmod: ""
+  },
+  {
+    init: "11",
+    race: "",
+    class: "",
+    type: "",
+    size: "",
+    dexmod: ""
+  },
+  {
+    init: "7",
+    race: "",
+    class: "",
+    type: "",
+    size: "",
+    dexmod: ""
+  },
+  {
+    init: "5",
+    race: "",
+    class: "",
+    type: "",
+    size: "",
+    dexmod: ""
+  },
+  {
+    init: "4",
+    race: "",
+    class: "",
+    type: "",
+    size: "",
+    dexmod: ""
+  },
+  {
+    init: "4",
+    race: "",
+    class: "",
+    type: "",
+    size: "",
+    dexmod: ""
+  },
+  {
+    init: "16",
+    race: "",
+    class: "",
+    type: "",
+    size: "",
+    dexmod: ""
+  },
+  {
+    init: "11",
+    race: "",
+    class: "",
+    type: "",
+    size: "",
+    dexmod: ""
+  },
+  {
+    init: "12",
+    race: "",
+    class: "",
+    type: "",
+    size: "",
+    dexmod: ""
+  }
+];
